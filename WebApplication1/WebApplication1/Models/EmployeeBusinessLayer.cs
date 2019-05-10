e30dfb2f7604c879563bb53b54d8fbe9695c8333
Francisco Peña <fa.pena@bi4group.com>
2019-05-10 11:50:18 +0200

    New project

---
+++ WebApplication1/WebApplication1/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.DataAccessLayer;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            SalesERPDAL salesDal = new SalesERPDAL();
+            return salesDal.Employees.ToList();
+
+            /*List<Employee>  employees = new List<Employee> ();
+            Employee emp = new Employee();
+            emp.FirstName = "johnson";
+            emp.LastName = " fernandes";
+            emp.Salary = 14000;
+            employees.Add(emp);
+
+            emp = new Employee();
+            emp.FirstName = "michael";
+            emp.LastName = "jackson";
+            emp.Salary = 16000;
+            employees.Add(emp);
+
+            emp = new Employee();
+            emp.FirstName = "robert";
+            emp.LastName = " pattinson";
+            emp.Salary = 20000;
+            employees.Add(emp);
+
+            return employees;*/
+        }
+
+        public Employee SaveEmployee(Employee e)
+        {
+            SalesERPDAL salesDal = new SalesERPDAL();
+            salesDal.Employees.Add(e);
+            salesDal.SaveChanges();
+            return e;
+        }
+
+        public UserStatus GetUserValidity(UserDetails u)
+        {
+            if(u.UserName == "Admin" && u.Password == "Admin")
+            {
+                return UserStatus.AuthenticatedAdmin;
+            }
+            else if (u.UserName == "Paco" && u.Password == "paco")
+            {
+                return UserStatus.AuthentucatedUser;
+            }
+            else
+            {
+                return UserStatus.NonAuthenticatedUser;
+            }
+        }
+    }
+}
